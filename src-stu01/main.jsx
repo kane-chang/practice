527b8476e7a0f6342f04dd315896bcd26591d833
Kane Chang <kane.chang.fl@gmail.com>
2024-01-11 19:20:04 +0000

    a

---
+++ src-stu01/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import App from './App';
+import './main.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import * as bootstrap from 'bootstrap'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+);
